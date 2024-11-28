5d458060e99693c7d8df50d1f9b3562ba9135054
RaissaBorgess <raissasantos123.rs33@gmail.com>
2024-11-28 11:37:21 -0300

    finalizado

--- EtecShopAPI/Controllers/ApiController.cs
+++ EtecShopAPI/Controllers/ApiController.cs
@@ -2,7 +2,6 @@ using EtecShopAPI.Data;
 using EtecShopAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace EtecShopAPI.Controllers;
 
@@ -21,7 +20,7 @@ public class CategoriasController(AppDbContext db) : ControllerBase
     [ProducesResponseType(404)]  
     public async Task<IActionResult> Get(int id)  =>
         _db.Categorias.Any(c => c.Id == id) ?
-        Ok(await _db.Categorias.FindAsync(id)) ;
+        Ok(await _db.Categorias.FindAsync(id)) :
         NotFound("Categoria não encontrada!");
     
     [HttpPost]
@@ -31,10 +30,39 @@ public class CategoriasController(AppDbContext db) : ControllerBase
     {
         if (!ModelState.IsValid)
             return BadRequest("Categoria informada com problemas");
+        if (_db.Categorias.Any(c => c.Nome == categoria.Nome))
+            return BadRequest($"Já existe uma Categoria com o nome ' {categoria.Nome}'");
         await _db.Categorias.AddAsync(categoria);
         await _db.SaveChangesAsync();
-        return CreatedAtAction(nameof(Get), categoria.Id [FromBody] Categoria categoria)
+        return CreatedAtAction(nameof(Get), categoria.Id, new { categoria });
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Edit(int id, [FromBody] Categoria categoria)
+    {
+        if (!ModelState.IsValid || id != categoria.Id)
+            return BadRequest("Categoria informada com problemas");
+        if (!_db.Categorias.Any(c => c.Id == id))
+            return NotFound("Categoria não encontrada!");
+        _db.Categorias.Update(categoria);
+        await _db.SaveChangesAsync();
+        return NoContent();
+        
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        Categoria categoria = _db.Categorias.Find(id);
+        if (categoria == null)
+        return NotFound("Categoria não encontrada!");
+        await _db.SaveChangesAsync();
+        return NoContent();
     }
 
     }
-}
